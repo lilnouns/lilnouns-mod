004b03c6db749dff1664587afc36e907cd60e0b2
Milad Nekofar <milad@nekofar.com>
2024-06-08 13:43:07 +0400

    refactor: replace let with const in api fetch
    The variables used in the API fetch operation were changed from 'let' to 'const'. This was done to improve code readability and maintainability, as these variables are not re-assigned after their initial declaration.

--- src/index.ts
+++ src/index.ts
@@ -29,8 +29,8 @@ export default {
     // publish to a Queue, query a D1 Database, and much more.
     //
     // We'll keep it simple and make an API call to a Cloudflare API:
-    let resp = await fetch('https://api.cloudflare.com/client/v4/ips')
-    let wasSuccessful = resp.ok ? 'success' : 'fail'
+    const resp = await fetch('https://api.cloudflare.com/client/v4/ips')
+    const wasSuccessful = resp.ok ? 'success' : 'fail'
 
     // You could store this result in KV, write to a D1 Database, or publish to a Queue.
     // In this template, we'll just log the result:
