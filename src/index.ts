4577989a86c43bbc165889387114d994648398ba
Milad Nekofar <milad@nekofar.com>
2024-06-09 07:08:46 +0400

    refactor: update scheduled method to fetch accounts at specific interval
    Updated the `scheduled` method to fetch 'lilnouns-accounts' from KeyValue storage only if the cron schedule is set to '30 * * * *'. This approach optimizes the data fetching process and ensures accounts are fetched precisely at the desired intervals, mitigating unnecessary data fetching and subsequent storage. This change aims to improve performance and efficiency while also managing resources optimally.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
 import { fetchAccounts } from './services/lilnouns/fetch-accounts'
 
 export default {
-  async scheduled(
-    controller: ScheduledController,
-    env: Env,
-    ctx: ExecutionContext,
-  ): Promise<void> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async scheduled(controller, env, _ctx): Promise<void> {
     const { KV } = env
 
-    let accounts = await env.KV.get('lilnouns-accounts', { type: 'json' })
-    if (accounts === null) {
-      accounts = await fetchAccounts()
+    if (controller.cron === '30 * * * *') {
+      let accounts = await env.KV.get('lilnouns-accounts', { type: 'json' })
+      if (accounts === null) {
+        accounts = await fetchAccounts()
 
-      await KV.put('lilnouns-accounts', JSON.stringify(accounts), {
-        expirationTtl: 60 * 60 * 24,
-      })
+        await KV.put('lilnouns-accounts', JSON.stringify(accounts), {
+          expirationTtl: 60 * 60 * 24,
+        })
+      }
+
+      console.log(accounts)
     }
-
-    console.log(accounts)
   },
 } satisfies ExportedHandler<Env>
