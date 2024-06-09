4a7d1b3137118638bdbaaca635a5aa0bddbc97f8
Milad Nekofar <milad@nekofar.com>
2024-06-09 23:57:16 +0400

    feat: add delegate fetching functionality to lilnouns service
    A new file has been introduced to the lilnouns service in order to fetch delegate information. This fetchDynamic 'fetchDelegates()' utilises GraphQL to retrieve delegate data, including id and related nouns' id. It continues fetching until no more data remains. It is important for accessing and manipulating data related to delegates for further use within the application.

---
+++ src/services/lilnouns/fetch-delegates.ts
@@ -0,0 +1,73 @@
+import { gql, GraphQLClient } from 'graphql-request'
+
+type Noun = {
+  id: string
+}
+
+type Delegate = {
+  id: string
+  nounsRepresented: Noun[]
+}
+
+type Data = {
+  delegates: Delegate[]
+}
+
+const nounSubgraphUrl =
+  'https://api.goldsky.com/api/public' +
+  '/project_cldjvjgtylso13swq3dre13sf/subgraphs/lil-nouns-subgraph/1.0.4/gn'
+
+const getDelegatesQuery = (skip: number, first: number) => gql`
+  query{
+    delegates(
+      skip: ${skip}
+      first: ${first}
+      orderBy: delegatedVotes
+      orderDirection: desc
+      where: { and: [{ delegatedVotes_gt: 0 }] }
+      subgraphError: deny
+    ) {
+      id
+      delegatedVotes
+      nounsRepresented(
+        skip: 0
+        first: 1000
+        orderBy: id
+        orderDirection: asc
+        where: {}
+      ) {
+        id
+      }
+    }
+  }
+`
+
+export async function fetchDelegates(): Promise<Delegate[]> {
+  const first = 1000
+  let skip = 0
+  let allDelegates: Delegate[] = []
+  let shouldContinueFetching = true
+
+  const client = new GraphQLClient(nounSubgraphUrl, {
+    errorPolicy: 'all',
+    fetch,
+  })
+
+  while (shouldContinueFetching) {
+    try {
+      const query = getDelegatesQuery(skip, first)
+      const { delegates } = await client.request<Data>(query)
+      shouldContinueFetching = delegates.length > 0
+
+      if (shouldContinueFetching) {
+        allDelegates = [...allDelegates, ...delegates]
+        skip += first
+      }
+    } catch (error) {
+      console.error('Error fetching delegates:', error)
+      shouldContinueFetching = false
+    }
+  }
+
+  return allDelegates
+}
