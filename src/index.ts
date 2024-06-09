e382e15ed4e0b58eebb8ab2a5ba16560b137b3df
Milad Nekofar <milad@nekofar.com>
2024-06-09 07:01:14 +0400

    refactor: update scheduled function and remove unnecessary comments
    The scheduled function in 'src/index.ts' was updated to handle fetching accounts. This involved removing the current code functionality and replacing it with fetching accounts from 'services/lilnouns/fetch-accounts' if null. The fetched accounts are then stored in KV with an expiration time to live of 24 hours. This change focuses on required functionality enhancement. Furthermore, extensive comments about Workers that initially existed in the code have been removed. This is done to declutter the code and facilitates readability by focusing only on necessary information.

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,22 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Run `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"` to see your
- *   worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a
- * type definition for the `Env` object can be regenerated with `npm run
- * cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { fetchAccounts } from './services/lilnouns/fetch-accounts'
 
 export default {
-  // The scheduled handler is invoked at the interval set in our wrangler.toml's
-  // [[triggers]] configuration.
   async scheduled(
     controller: ScheduledController,
     env: Env,
     ctx: ExecutionContext,
   ): Promise<void> {
-    // A Cron Trigger can make requests to other endpoints on the Internet,
-    // publish to a Queue, query a D1 Database, and much more.
-    //
-    // We'll keep it simple and make an API call to a Cloudflare API:
-    const resp = await fetch('https://api.cloudflare.com/client/v4/ips')
-    const wasSuccessful = resp.ok ? 'success' : 'fail'
+    const { KV } = env
 
-    // You could store this result in KV, write to a D1 Database, or publish to a Queue.
-    // In this template, we'll just log the result:
-    console.log(`trigger fired at ${controller.cron}: ${wasSuccessful}`)
+    let accounts = await env.KV.get('lilnouns-accounts', { type: 'json' })
+    if (accounts === null) {
+      accounts = await fetchAccounts()
+
+      await KV.put('lilnouns-accounts', JSON.stringify(accounts), {
+        expirationTtl: 60 * 60 * 24,
+      })
+    }
+
+    console.log(accounts)
   },
 } satisfies ExportedHandler<Env>
