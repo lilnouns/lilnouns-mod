5b1eba3ad91b2efde12dd31a01acf6b9cf8c1413
Milad Nekofar <milad@nekofar.com>
2024-06-15 14:53:16 +0400

    feat(warpcast): add `fetchFeedItems` function
    This commit introduces the `fetchFeedItems` function in the warpcast service. This function is responsible for fetching feed items from the server using inputted arguments. The function then returns the fetched items or an empty array when an error occurs.

---
+++ src/services/warpcast/fetch-feed-items.ts
@@ -0,0 +1,48 @@
+import { Item } from './index'
+
+interface Response {
+  result: {
+    items: Item[]
+    latestMainCastTimestamp: number
+    feedTopSeenAtTimestamp: number
+    replaceFeed: boolean
+  }
+}
+
+type FetchFeedItemsArgs = {
+  feedKey: string
+  feedType: string
+  olderThan?: number
+  latestMainCastTimestamp?: number
+  excludeItemIdPrefixes?: string[]
+  viewedCastHashes: string
+  updateState: boolean
+}
+
+export async function fetchFeedItems(args: FetchFeedItemsArgs): Promise<Item[]> {
+  const url = 'https://client.warpcast.com/v2/feed-items'
+  const headers = {
+    Authorization: 'Bearer MK-',
+    'Content-Type': 'application/json; charset=utf-8',
+  }
+  const body = JSON.stringify(args)
+
+  const options: RequestInit = {
+    method: 'POST',
+    headers,
+    body,
+  }
+
+  try {
+    const response = await fetch(url, options)
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const data = await response.json<Response>()
+
+    return data.result.items as Item[]
+  } catch (error) {
+    console.error('Error fetching feed items:', error)
+    return []
+  }
+}
