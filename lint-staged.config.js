809729b0fc78226ff54de2cdf7ec594150c680c3
Milad Nekofar <milad@nekofar.com>
2024-06-15 09:40:44 +0400

    chore(lint-staged): add toml files to prettier formatting
    This change was made to ensure toml files are also formatted using prettier in the lint-staged configuration.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,5 @@ export default {
   '*.{js,jsx, ts,tsx}': ['eslint --fix', 'prettier --write'],
   '*.{json,yaml,yml}': ['prettier --write'],
   '*.{md,mdx}': ['prettier --write'],
+  '*.{toml}': ['prettier --write'],
 }
