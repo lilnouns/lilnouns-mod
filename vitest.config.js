c061c23e91b44efe382977c2a8e34e50bfd60f83
Milad Nekofar <milad@nekofar.com>
2024-06-15 14:01:56 +0400

    chore(vitest): add resolve alias to vitest configuration
    An alias for root source directory has been defined to make imports in the test configuration simpler. This helps in shortening paths when importing source files in tests.

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,17 @@
 import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
 
+/** @type {import('vite').UserConfig} */
 export default defineWorkersConfig({
   test: {
     poolOptions: {
       workers: {
         wrangler: { configPath: './wrangler.toml' },
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
     },
   },
 })
