6b19426a7bcb67c6926cb2cea42c06abf424cb05
Milad Nekofar <milad@nekofar.com>
2024-06-17 13:12:34 +0400

    test: remove unnecessary eslint-disable comment in 'cloudflare:test' module
    Removed the 'eslint-disable-next-line @typescript-eslint/no-empty-interface' comment from the 'cloudflare:test' module as it was redundant. The linting rule it was disabling is not violated in this context, so no need to suppress it.

--- test/env.d.ts
+++ test/env.d.ts
@@ -1,5 +1,4 @@
 declare module 'cloudflare:test' {
   // Controls the type of `import("cloudflare:test").env`
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface ProvidedEnv extends Env {}
 }
