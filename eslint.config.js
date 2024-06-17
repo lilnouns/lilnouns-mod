7331857c099eaf8db8b2a4a4c1e0a785ae1eb0a9
Milad Nekofar <milad@nekofar.com>
2024-06-17 13:54:45 +0400

    chore(eslint): enhance configurations for better linting
    Updated eslint configurations to integrate new plugins, improve code checking, and address specific file types. These changes improve the quality of linting, particularly in TypeScript files. The additional plugins and configurations correspondingly enhance the detection of potential issues and stylistic inconsistencies in the codebase.

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,54 @@
 import eslint from '@eslint/js'
 import prettierConfig from 'eslint-config-prettier'
+import jsdoc from 'eslint-plugin-jsdoc'
 import prettier from 'eslint-plugin-prettier'
 import unicorn from 'eslint-plugin-unicorn'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+  // config with just ignores is the replacement for `.eslintignore`
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/test/*',
+      '**/*.config.js',
+    ],
+  },
+
+  // extends ...
   eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  ...[prettierConfig],
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  ...[prettierConfig, jsdoc.configs['flat/recommended-typescript-error']],
+
+  // base config
   {
-    ignores: ['test/*', 'dist/*', '**/*.config.js'],
-  },
-  {
-    plugins: {
-      prettier,
+    languageOptions: {
+      globals: {
+        ...globals.es2022,
+        ...globals.node,
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+        allowAutomaticSingleRunInference: true,
+      },
     },
-  },
-  {
     plugins: {
       unicorn,
+      prettier,
     },
     rules: {
       ...unicorn.rules.recommended,
       'unicorn/prevent-abbreviations': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
     },
+  },
+
+  {
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
   },
 )
