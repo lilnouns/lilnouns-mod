b2d0b4bf65a1a9f8235f7e1377ec58316ab4754d
Milad Nekofar <milad@nekofar.com>
2024-06-17 10:34:45 +0400

    test: add eslint disable line for empty interface in module
    Added a line to disable an eslint rule that disallows empty interfaces in the 'cloudflare:test' module. This line was necessary to maintain code cleanliness while ensuring we respect development environment constraints.

--- test/env.d.ts
+++ test/env.d.ts
@@ -1,4 +1,5 @@
 declare module 'cloudflare:test' {
   // Controls the type of `import("cloudflare:test").env`
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface ProvidedEnv extends Env {}
 }
