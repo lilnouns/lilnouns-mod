be6b7f48a51946a3bbcf2443ea59942f82c91108
Milad Nekofar <milad@nekofar.com>
2024-06-24 15:56:12 +0400

    refactor(warpcast): simplify `likeCast` function
    Refactored the `likeCast` function to use the fetchRequest helper and get its configs from a centralized location. This streamlines the request process and makes the function easier to understand and maintain.

--- src/services/warpcast/like-cast.ts
+++ src/services/warpcast/like-cast.ts
@@ -1,3 +1,5 @@
+import { configs } from '@/configs'
+import { fetchRequest, HttpRequestMethod } from '@/services/warpcast/index'
 import { Like } from '@/services/warpcast/types'
 
 interface Response {
@@ -5,33 +7,19 @@ interface Response {
     like: Like
   }
 }
-/**
- * Sends a request to like a cast using the provided castHash and token.
- * @param castHash - The hash of the cast to be liked.
- * @returns - A promise that resolves with the response from the server.
- * @throws {Error} - If the request fails, an error is thrown.
- */
-export async function likeCast(castHash: string): Promise<Response> {
-  const url = 'https://client.warpcast.com/v2/cast-likes'
-  const body = JSON.stringify({ castHash })
-  const headers = {
-    Authorization: `Bearer `,
-    'Content-Type': 'application/json; charset=utf-8',
-  }
 
-  const options: RequestInit = {
-    method: 'PUT',
-    headers,
-    body,
-  }
-
-  const response = await fetch(url, options)
-
-  if (!response.ok) {
-    throw new Error(
-      `Failed to like cast with hash ${castHash}: ${response.statusText}`,
-    )
-  }
-
-  return await response.json()
+export const likeCast = async (
+  env: Env,
+  castHash: string,
+): Promise<Response> => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { warpcastAccessToken, warpcastBaseUrl } = configs(env)
+  const body = { castHash }
+  return await fetchRequest<Response>(
+    warpcastBaseUrl,
+    warpcastAccessToken,
+    HttpRequestMethod.PUT,
+    '/cast-likes',
+    { json: body },
+  )
 }
