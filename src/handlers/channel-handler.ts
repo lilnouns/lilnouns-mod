5a8c88ac71f709d82bc2eed612d3ddda0a80c0de
Milad Nekofar <milad@nekofar.com>
2024-06-24 19:02:44 +0400

    feat(channel-handler): add conditions for liking content
    The channel-handler now automatically likes content if the item's author is a certain owner or if an item's reaction count exceeds a certain limit. It also checks if the owner has liked the items with reactions before liking them. This enhancement allows for more precise control over content interaction in the application.

--- src/handlers/channel-handler.ts
+++ src/handlers/channel-handler.ts
@@ -1,5 +1,6 @@
 import { likeCast } from '@/services/warpcast'
 import { fetchFeedItems } from '@/services/warpcast/fetch-feed-items'
+import { getCastLikes } from '@/services/warpcast/get-cast-likes'
 
 /**
  * Handles the channel based on the given environment.
@@ -7,6 +8,7 @@ import { fetchFeedItems } from '@/services/warpcast/fetch-feed-items'
  * @returns - A Promise that resolves when the channel handler has completed execution.
  */
 export async function channelHandler(env: Env) {
+  const owner = 'nekofar.eth'
   const items = await fetchFeedItems({
     feedKey: 'lilnouns',
     feedType: 'unfiltered',
@@ -15,12 +17,21 @@ export async function channelHandler(env: Env) {
   })
 
   for (const item of items) {
-    if (item.cast.author.username == 'nekofar.eth') {
+    if (item.cast.author.username == owner) {
       await likeCast(env, item.cast.hash)
-    }
+    } else if (item.cast.reactions.count > 5) {
+      await likeCast(env, item.cast.hash)
+    } else if (item.cast.reactions.count > 0) {
+      const {
+        result: { likes },
+      } = await getCastLikes(env, item.cast.hash)
 
-    if (item.cast.reactions.count > 5) {
-      await likeCast(env, item.cast.hash)
+      for (const like of likes) {
+        if (like.reactor.username != owner) {
+          continue
+        }
+        await likeCast(env, item.cast.hash)
+      }
     }
   }
 }
