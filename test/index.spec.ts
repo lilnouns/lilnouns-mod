cedf475ec733229442a8ef43666a5c052a2051f2
Milad Nekofar <milad@nekofar.com>
2024-07-14 23:01:03 +0400

    test: update cron expression in scheduled controller test
    The cron expression in the scheduled controller test was modified to run every minute for better test coverage.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -12,7 +12,7 @@ import worker from '../src'
 it('calls scheduled handler', async () => {
   const ctrl = createScheduledController({
     scheduledTime: new Date(1000),
-    cron: '0 */12 * * *',
+    cron: '* * * * *',
   })
   const ctx = createExecutionContext()
   await worker.scheduled(ctrl, env, ctx)
