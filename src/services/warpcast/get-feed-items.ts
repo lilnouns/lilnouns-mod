f7ac016d0383c4b45f881f3dfe4e5676b13bd785
Milad Nekofar <milad@nekofar.com>
2024-09-28 21:47:04 +0400

    refactor(warpcast): rename optional param in `getFeedItems`
    Renamed `olderThan` to `excludeItemIdPrefixes` for clarity.

--- src/services/warpcast/get-feed-items.ts
+++ src/services/warpcast/get-feed-items.ts
@@ -13,22 +13,22 @@ interface Response {
 }
 
 /**
- * Fetches feed items based on the provided feed key, feed type, and an optional timestamp.
- * @param env - The environment variables required to make the request, including access token and base URL.
- * @param feedKey - The key identifying the feed to be fetched.
+ * Fetches feed items based on the given parameters.
+ * @param env - The environment variables containing access token and base URL.
+ * @param feedKey - The key identifying the feed to retrieve items from.
  * @param feedType - The type of the feed.
- * @param [olderThan] - Optional timestamp to fetch items older than the specified time.
- * @returns - A promise that resolves to a Result object containing the feed items.
+ * @param [excludeItemIdPrefixes] - Optional prefixes to exclude specific item IDs.
+ * @returns - A promise that resolves to the result of the request.
  */
 export const getFeedItems = async (
   env: Env,
   feedKey: string,
   feedType: string,
-  olderThan?: number,
+  excludeItemIdPrefixes?: string[],
 ): Promise<Result> => {
   const { WARPCAST_ACCESS_TOKEN: accessToken, WARPCAST_BASE_URL: baseUrl } = env
 
-  const body = { feedKey, feedType, olderThan }
+  const body = { feedKey, feedType, excludeItemIdPrefixes }
 
   const { result } = await fetchRequest<Response>(
     baseUrl,
