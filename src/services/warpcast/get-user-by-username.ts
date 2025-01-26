9670d5a8af88fca4f999df67946b8e2ae6a1d171
Milad Nekofar <milad@nekofar.com>
2025-01-26 20:52:28 +0300

    refactor(warpcast): simplify `getUserByUsername` implementation
    improved the method by enhancing `extras` structure and adding
    parameter descriptions. this improves code readability and
    maintainability.

--- src/services/warpcast/get-user-by-username.ts
+++ src/services/warpcast/get-user-by-username.ts
@@ -1,35 +1,41 @@
 import { fetchRequest, HttpRequestMethod } from '@/services/warpcast/index'
 import { User } from '@/services/warpcast/types'
 
-interface Extras {
-  fid: number
-  custodyAddress: string
-}
-
 interface Result {
   user: User
+  extras: {
+    fid: number
+    custodyAddress: string
+    ethWallets: string[]
+    solanaWallets: string[]
+  }
   collectionsOwned: string[]
-  extras: Extras
 }
 
 interface Response {
   result: Result
 }
 
+/**
+ * Retrieves user information based on the provided username.
+ * @param env - The environment object containing necessary configuration values.
+ * @param username - The username for which the user information is to be fetched.
+ * @returns A promise that resolves to the result containing user information.
+ */
 export const getUserByUsername = async (
   env: Env,
   username: string,
 ): Promise<Result> => {
   const { WARPCAST_ACCESS_TOKEN: accessToken, WARPCAST_BASE_URL: baseUrl } = env
 
+  const params = { username }
+
   const { result } = await fetchRequest<Response>(
     baseUrl,
     accessToken,
     HttpRequestMethod.GET,
-    '/v2/user-by-username',
-    {
-      params: { username },
-    },
+    `/v2/user-by-username`,
+    { params },
   )
 
   return result
