35525d4ce8a3d85ecd3880fb03a2bc92134ca189
Milad Nekofar <milad@nekofar.com>
2025-09-23 01:12:04 +0300

    chore(migrations): update account table with indexes
    Added indexes for `fid`, `fname`, and `ens` fields to optimize query performance and enhance database efficiency.

--- migrations/0001_create_account_table.sql
+++ migrations/0001_create_account_table.sql
@@ -1,1 +1,18 @@
--- Migration number: 0001 	 2025-09-22T21:54:11.086Z
+-- CreateTable
+CREATE TABLE "Account" (
+    "address" TEXT NOT NULL PRIMARY KEY,
+    "fid" INTEGER,
+    "fname" TEXT,
+    "ens" TEXT,
+    "pfpUrl" TEXT,
+    "updatedAt" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE INDEX "Account_fid_idx" ON "Account"("fid");
+
+-- CreateIndex
+CREATE INDEX "Account_fname_idx" ON "Account"("fname");
+
+-- CreateIndex
+CREATE INDEX "Account_ens_idx" ON "Account"("ens");
